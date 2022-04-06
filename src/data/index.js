d9edc2574e6b83cf9936f0262c9b20016fb831b0
Niranjan Adhikari <niranjanadhikari2053@gmail.com>
2022-04-06 21:58:41 +0545

    changed companies logos

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
-
 import graphicdesign from "../images/graphicdesign.svg";
 import UXDesigner from "../images/uiuxdesign.svg";
 import UIDev from "../images/uidev.svg";
@@ -39,7 +38,7 @@ const jobExperience = [
     role: "UX Designer",
     company: "Optimum Futurist",
     companyLogo:
-      "https://media-exp1.licdn.com/dms/image/C4D0BAQEy60nl7vJ4IA/company-logo_100_100/0/1609958980130?e=1645660800&v=beta&t=aKx9DTUKDtUc8UgmS04_Z6nCvcz-zBhCIE_rdjOxxY0",
+      "https://d25hn4jiqx5f7l.cloudfront.net/companies/logos/medium/optimum-futurist-tech-inc_1613727488.png?1613727488",
     from: "Mar 2021",
     to: "present",
     type: "Full-time",
@@ -49,8 +48,7 @@ const jobExperience = [
     id: "2",
     role: "UI Developer",
     company: "Bentray Technologies",
-    companyLogo:
-      "https://media-exp1.licdn.com/dms/image/C560BAQHJMBV1Ai2_9Q/company-logo_100_100/0/1519888978535?e=1645660800&v=beta&t=YHbcC1Sto3xylL7t7ZsjDOBQtoTnYq8ag2Lkvc2TD9o",
+    companyLogo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR54En3GMb-t6CJWsFSjiVUX5xZA10W0Aso5E03N5mOjZU4nF10KGKGifBOjaYI7Ae5fqs&usqp=CAU",
     from: "Jan 2020",
     to: "Feb 2021",
     type: "Full-time",
@@ -61,7 +59,7 @@ const jobExperience = [
     role: "Graphic Designer",
     company: "Digital Ghumti",
     companyLogo:
-      "https://media-exp1.licdn.com/dms/image/C560BAQFrPLdN-6NFlw/company-logo_100_100/0/1588853092675?e=1645660800&v=beta&t=rdWBRABcANXLE8_BvLEfMF-VzdpKutm8zDrcETvzLRo",
+      "https://i.pinimg.com/280x280_RS/56/a5/bb/56a5bbc2d0f21b83dd10d63355f0e8e7.jpg",
     from: "May 2020",
     to: "Jul 2020",
     type: "Part-time",
@@ -131,47 +129,54 @@ const jobRoles = [
 ];
 
 const academics = [
-    {
-      id: 1,
-      course: "B.E Computer",
-      duration: "2015-2019",
-      academy: "Pashchimanchal Campus, IOE Tribhuwan University",
-    },
-    {
-      id: 2,
-      course: "+2 Science",
-      duration: "2013-2015",
-      academy: "Kathmandu Model HSS",
-    },
-    {
-      id: 3,
-      course: "SLC (Nepal Board)",
-      duration: "2000-2013",
-      academy: "Canon HSS",
-    },
-  ];
+  {
+    id: 1,
+    course: "B.E Computer",
+    duration: "2015-2019",
+    academy: "Pashchimanchal Campus, IOE Tribhuwan University",
+  },
+  {
+    id: 2,
+    course: "+2 Science",
+    duration: "2013-2015",
+    academy: "Kathmandu Model HSS",
+  },
+  {
+    id: 3,
+    course: "SLC (Nepal Board)",
+    duration: "2000-2013",
+    academy: "Canon HSS",
+  },
+];
 
-  const certifications = [
-    {
-      id: 1,
-      name: "UX driven software design",
-      issuedFrom: "Pluralsight",
-    },
-    {
-      id: 2,
-      name: "Visual Elements of User Interface Design",
-      issuedFrom: "Calarts (Coursera)",
-    },
-    {
-      id: 3,
-      name: "UX Design Fundamentals",
-      issuedFrom: "Calarts (Coursera)",
-    },
-    {
-      id: 4,
-      name: "Textual Elements of Design: Fonts, Typography, and Spacing ",
-      issuedFrom: "University of Colorado (Coursera)",
-    },
-  ];
+const certifications = [
+  {
+    id: 1,
+    name: "UX driven software design",
+    issuedFrom: "Pluralsight",
+  },
+  {
+    id: 2,
+    name: "Visual Elements of User Interface Design",
+    issuedFrom: "Calarts (Coursera)",
+  },
+  {
+    id: 3,
+    name: "UX Design Fundamentals",
+    issuedFrom: "Calarts (Coursera)",
+  },
+  {
+    id: 4,
+    name: "Textual Elements of Design: Fonts, Typography, and Spacing ",
+    issuedFrom: "University of Colorado (Coursera)",
+  },
+];
 
-export { heroData, contactList, jobExperience, jobRoles, academics, certifications };
+export {
+  heroData,
+  contactList,
+  jobExperience,
+  jobRoles,
+  academics,
+  certifications,
+};
